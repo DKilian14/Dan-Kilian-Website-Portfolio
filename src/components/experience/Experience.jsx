41f28b55ae756c560aa53db08b160d20ecb87c99
Dan Kilian <dankilian@yahoo.com>
2022-07-08 16:58:45 -0500

    revised the experience component to match my skills

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -41,20 +41,6 @@ const Experience = () => {
             <article className='experience__details'>
               <BsPatchCheckFill className={patchCheckFill}  />
               <div>
-                <h4>Bootstrap</h4>
-                {/* <small className='text-light'>Experienced</small> */}
-              </div>
-            </article>
-            <article className='experience__details'>
-              <BsPatchCheckFill className={patchCheckFill}  />
-              <div>
-                <h4>Tailwind</h4>
-                {/* <small className='text-light'>Experienced</small> */}
-              </div>
-            </article>
-            <article className='experience__details'>
-              <BsPatchCheckFill className={patchCheckFill}  />
-              <div>
                 <h4>React</h4>
                 {/* <small className='text-light'>Experienced</small> */}
               </div>
@@ -72,20 +58,6 @@ const Experience = () => {
               <BsPatchCheckFill className={patchCheckFill}  />
               <div>
                 <h4>Node JS</h4>
-                {/* <small className='text-light'>Experienced</small> */}
-              </div>
-            </article>
-            <article className='experience__details'>
-              <BsPatchCheckFill className={patchCheckFill}  />
-              <div>
-                <h4>MongoDB</h4>
-                {/* <small className='text-light'>Intermediate</small> */}
-              </div>
-            </article>
-            <article className='experience__details'>
-              <BsPatchCheckFill className={patchCheckFill}  />
-              <div>
-                <h4>PHP</h4>
                 {/* <small className='text-light'>Experienced</small> */}
               </div>
             </article>
