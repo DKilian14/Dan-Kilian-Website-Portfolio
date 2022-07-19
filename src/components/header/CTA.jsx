c0637aa4e370b884d18ad16b32e3f1bc7aca40f1
Dan Kilian <dankilian@yahoo.com>
2022-07-18 20:23:03 -0500

    changed portfolio component

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import CV from '../../assets/cv.pdf'
+import CV from '../../assets/Daniel_Kilian_Resume.pdf'
 
 const CTA = () => {
   return (
--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -6,9 +6,9 @@ import {FaDiscord} from 'react-icons/fa'
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href="https://linkedin.com" target="_blank"><BsLinkedin/></a>
-        <a href="https://github.com" target="_blank"><FaGithub/></a>
-        <a href="https://discord.com/app" target="_blank"><FaDiscord/></a>
+        <a href="https://www.linkedin.com/in/daniel-kilian-b69606113/" target="_blank"><BsLinkedin/></a>
+        <a href="https://github.com/DKilian14" target="_blank"><FaGithub/></a>
+        {/* <a href="https://discord.com/app" target="_blank"><FaDiscord/></a> */}
     </div>
   )
 }
